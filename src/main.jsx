52ce32759a6fc29bd9c99622c89096bc0c6b59c4
n58s29 <n58s29@pm.me>
2026-03-29 15:03:44 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import MieuxDemain from './App.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <MieuxDemain />
+  </React.StrictMode>,
+)
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import GutFeel from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
